22ea044f7de1d4f7e9f126173aea1525004337b6
gofreish <85355218+gofreish@users.noreply.github.com>
2024-04-29 12:56:08 +0000

    Ajout de la fonction de calcul de la superficie

--- src/main/java/com/mycompany/javalearning/JavaLearning.java
+++ src/main/java/com/mycompany/javalearning/JavaLearning.java
@@ -13,4 +13,14 @@ public class JavaLearning {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    /**
+     * Calcule la superficie du rectangle.
+     * @param longueur
+     * @param largeur
+     * @return float la superficie du rectangle
+     */
+    public float calculerSuperficieRectangle(final float longueur, final float largeur){
+        return longueur*largeur;
+    }
 }
